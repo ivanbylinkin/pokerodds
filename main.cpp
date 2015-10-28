3916de330678d2554d2ab5363b8247e424633188
Ivan <ivanbylinkin@me.com>
2015-10-28 11:36:32 -0400

    Update main.cpp
    finished the deck shuffle/create function

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@ class card{
 public:
 	enum suit { clubs, diamonds, hearts, spades };
 
-	suit show_suit(){ return s;  }
+	suit show_suit(){ return s; }
 	int show_value(){ return value; }
-	
+
 	void create_card(){
 		s = rand_suit();
-		value = rand() % 14;
+		value = (rand() % 13) + 1;
 	}
 private:
 	suit s;
 	int value;
-	
+
 	suit rand_suit(){
 		suit arr[4] = { clubs, diamonds, hearts, spades };
 		int r = rand() % 4;
@@ -33,39 +33,58 @@ private:
 // deck contains suffles and deals
 class deck{
 public:
-	void create_deck(){
+	enum suit { clubs, diamonds, hearts, spades };
+	// decks are randomly generated meaning each shuffle is a new random deck
+	void shuffle(){
 		// while loop will ensure it will keep going until deck is properly filled
-		int deck_size = 0, club_size = 0, diamond_size = 0, spade_size = 0;
-		int clubs[13], diamonds[13], hearts[13], spades[13];
-		int deck_cards = { clubs, diamonds, hearts, spades };
-		bool indexOf(int value, int arr[], int size){
-			for (int i = 0; i < size; i++){
-				if (arr[i] == value){ return true; }
-			}
-			return false;
-		}
+		int deck_size = 0;
+		int counts[4][13];
+		// nullify arrays to use for checks
+		nullifyArr(counts[clubs], 13);
+		nullifyArr(counts[diamonds], 13);
+		nullifyArr(counts[hearts], 13);
+		nullifyArr(counts[spades], 13);
+
 		while (deck_size < 52){
 			// initialize a card
 			card tmp;
 			tmp.create_card();
-			// check current array
+			// check current card
+			int value = tmp.show_value(),
+			card_suit = tmp.show_suit();
+			// check index of array
+			bool exists = counts[card_suit][value] == -1 ? false : true;
+			if (!exists){
+				dck[deck_size] = tmp;
+				deck_size++;
+			}
+		}
+	}
 
+	void show_deck(){
+		for (int i = 0; i < 52; i++){
+			cout << "Suit: " << dck[i].show_suit() << " Value: " << dck[i].show_value() << endl;
 		}
 	}
 private:
 	card dck[52];
+
+	void nullifyArr(int arr[], int n){
+		for (int i = 0; i < n; i++){
+			arr[i] = -1;
+		}
+	}
 };
 
 // main is used to seed the random numbers
 // main also makes the calls to functions
 int main(){
-
-	for (int i = 0; i < 52; i++){
-		card tst;
-		tst.create_card();
-
-		cout << "card suit: " << tst.show_suit() << " card value: " << tst.show_value() << endl;
-	}
+	// seed the generator
+	srand(time(NULL));
+	// make the deck
+	deck newDeck;
+	newDeck.shuffle();
+	newDeck.show_deck();
 
 	cout << "Please press enter to continue..." << endl;
 	getchar();
